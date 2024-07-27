b21dbbde237c3e3c8ee9773fb4a9be63382ac85d
ikunstoppable <wangwang357@yeah.net>
2024-07-27 14:54:48 +0800

    js和css文件打包文件加时间

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
+const moment = require('moment')
+const date = new moment().format('YYYYMMDDHHmmss')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // js 打包文件加上时间
+  configureWebpack: config => {
+    if (process.env.NODE_ENV==='production') {
+      config.output.filename = `js/[name].${date}.js`
+      config.output.chunkFilename = `js/[name].${date}.js`
+    }
+  },
+  // css 打包文件加上时间
+  css: {
+    extract: {
+      filename: `css/[name].${date}.css`,
+      chunkFilename: `css/[name].${date}.css`
+    }
+  }
 })
